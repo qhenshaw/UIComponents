04f4bd8c9803f402ad15dc578b3c444fd6113ae8
Quin Henshaw <qhenshaw@gmail.com>
2022-11-16 15:45:15 -0800

    - fixed GUI sounds ignoring mixer group volume - added sound to slider movement

--- Runtime/UnityAudioPlayer.cs
+++ Runtime/UnityAudioPlayer.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 namespace UIComponents
 {
     [CreateAssetMenu(menuName = "Unity Audio Player")]
     public class UnityAudioPlayer : ScriptableObject
     {
+        [SerializeField] private AudioMixerGroup _mixerGroup;
+
         public void PlayOneShot2D(AudioClip audioClip)
         {
             GameObject go = new GameObject($"Audio: {audioClip.name}");
             AudioSource source = go.AddComponent<AudioSource>();
             source.playOnAwake = false;
+            source.outputAudioMixerGroup = _mixerGroup;
             source.clip = audioClip;
             source.loop = false;
             source.spatialBlend = 0f;
